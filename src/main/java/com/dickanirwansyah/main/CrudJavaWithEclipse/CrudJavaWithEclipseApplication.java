9c5c4aa8b85dc02cd586e95f0c14e4d9e524bb2e
dickanirwansyah <dickanirwansyah@gmail.com>
2017-06-05 02:28:16 -0400

    Main java

---
+++ src/main/java/com/dickanirwansyah/main/CrudJavaWithEclipse/CrudJavaWithEclipseApplication.java
@@ -0,0 +1,12 @@
+package com.dickanirwansyah.main.CrudJavaWithEclipse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudJavaWithEclipseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudJavaWithEclipseApplication.class, args);
+	}
+}
